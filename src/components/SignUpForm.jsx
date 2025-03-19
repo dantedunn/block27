e901cddc2c9f0aa228f349d63e003c8bfc3cc5ac
Dunn, Dante <DanteDunn@quickenloans.com>
2025-03-19 16:24:22 -0400

    commit from work

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,11 +5,26 @@ function SignUpForm() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+
+async function handleSubmit(event) {
+  event.preventDefault();
+console.log("G'day, Mate!")
+}
+
+
   return (
     //Return needs a parent of an h2/form like div. HTML element as a parent...
     <div>
       <h2>"Sign Up!"</h2>
-      <form></form>
+      <form onSubmit={(e)=> {handleSubmit(e) }}>
+        <label>
+          username: <input  onChange={(event) => setUserName(event.target.value)} />
+        </label>
+        <label >
+          password: <input onChange={(event) => setPassword(event.target.value)} />
+        </label>
+        <button id="subButton" >Submit</button>
+      </form>
     </div>
   );
 }
