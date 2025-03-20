507609f12ac1a8f3ef99454cf3560e0af87e17a0
dantedunn <dantedunn@gmail.com>
2025-03-19 22:44:37 -0400

    signup done

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,25 +5,49 @@ function SignUpForm() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userName: <input />,
+            password: <input />,
+          }),
+        }       
+      ); 
+      const data = await response.json()
+      console.log(data)
 
-async function handleSubmit(event) {
-  event.preventDefault();
-console.log("G'day, Mate!")
-}
-
+    } catch (error) {
+      setError(error.message)
+    }
+  }
 
   return (
     //Return needs a parent of an h2/form like div. HTML element as a parent...
     <div>
       <h2>"Sign Up!"</h2>
-      <form onSubmit={(e)=> {handleSubmit(e) }}>
+      {}
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
         <label>
-          username: <input  onChange={(event) => setUserName(event.target.value)} />
+          username:{" "}
+          <input onChange={(event) => setUserName(event.target.value)} />
         </label>
-        <label >
-          password: <input onChange={(event) => setPassword(event.target.value)} />
+        <label>
+          password:{" "}
+          <input onChange={(event) => setPassword(event.target.value)} />
         </label>
-        <button id="subButton" >Submit</button>
+        <button id="subButton">Submit</button>
       </form>
     </div>
   );
